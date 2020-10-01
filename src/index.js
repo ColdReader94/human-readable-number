47e47cc279b3c80b5bcbf0f2ad782bd86fe25c33
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-10-01 10:49:07 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -2,33 +2,41 @@ module.exports = function toReadable (number) {
     let string = number.toString();
     let output = [];
     // до 20
-    let numberArr = {0: 'null', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: "eight", 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelv', 13: 'thirdteen', 14: 'fourteen', 15: 'fiveteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
+    let numberArr = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: "eight", 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
    //десятки от 20
    let numberArr2 = { 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'};
 
-   if(number < 19) {
+   if(number <= 19) {
     output.push(numberArr[number]);
    }
 
     if(number > 19 && number < 100) {
-       let tens = number % 10;
-        let ones = number - (tens * 10);
-        output.push(numberArr2[tens]);        
-        output.push(numberArr[ones]);
+       let tens = Math.trunc(number / 10) * 10;
+        let ones = number - tens;
+        output.push(numberArr2[tens]);
+         if(string[string.length - 1] !== '0') {
+            output.push(numberArr[ones]);
+        }        
     }
     
-    if(number > 100) {
-        let hundreds = number % 100;
-        let tensMore = number - (hundreds * 100);
-        let tens = tensMore % 10;
-        let ones = tensMore - (tens * 10);
+    if(number >= 100) {
+        let hundreds = Math.trunc(number / 100);
         output.push(numberArr[hundreds]);
         output.push('hundred');
+        if (string[string.length - 2] == '0' && string[string.length - 1] == '0') {
+            return output.join(' ');
+        } else {
+        let tensMore = number - (hundreds * 100);
+        if(tensMore > 19) {
+        let tens = Math.trunc(tensMore / 10) * 10;
+        let ones = tensMore - tens;
         output.push(numberArr2[tens]);
-        output.push(numberArr[ones]);
-     }
-
-     
+            if(string[string.length - 1] !== '0') {
+                output.push(numberArr[ones])
+            }        
+        } else output.push(numberArr[tensMore]);           
+    }
+}
      return output.join(' ');
 }
     
